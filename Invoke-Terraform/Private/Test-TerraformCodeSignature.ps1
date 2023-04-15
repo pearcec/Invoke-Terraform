ac41f97d0be7267a4fd0716a62e50a75f4afaa23
Christian Pearce <cpearce@stackoverflow.com>
2023-04-15 16:55:58 -0400

    .

--- Invoke-Terraform/Private/Test-TerraformCodeSignature.ps1
+++ Invoke-Terraform/Private/Test-TerraformCodeSignature.ps1
@@ -14,6 +14,9 @@ Function Test-TerraformCodeSignature {
         $tfThumbprint = (Get-AuthenticodeSignature -FilePath (Get-TerraformPath -TFVersion $TFVersion)).SignerCertificate.Thumbprint
         $configTFThumbprint = (Get-TerraformConfiguration).HashiCorpWindowsThumbprint
 
+        Write-Debug -Message "Terraform thumbprint: $tfThumbprint"
+        Write-Host -ForegroundColor Yellow -Object "Terraform thumbprint: $tfThumbprint"
+
         if ($configTFThumbprint -is [String]) {
             # The configuration is a string
             return ($configTFThumbprint -eq $tfThumbprint)
